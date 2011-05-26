f0cd9cfddeec3dc4b3fd36a46ca48c1f32db37e4
Makis Tracend <makis.tracend@gmail.com>
2011-05-26 18:40:09 +1000

    Included color and label for the polygon

--- kml2as/index.php
+++ kml2as/index.php
@@ -1,20 +1,46 @@
 <?php
-$xmlUrl = "example.kml"; // XML feed file/URL
+// there is a known issue with the kml namespace on the exported file - manually removing the namespaces should let the parsing work properly
+$xmlUrl = "example.kml"; 
 $xmlStr = file_get_contents($xmlUrl);
 $xml = simplexml_load_string($xmlStr);
 
-//$placemarks = $xml->xpath("/kml/Document/Folder/Placemark");
-//$styles = $xml->xpath("/kml/Document/StyleMap");
-$polygons = $xml->xpath("/kml/Document/Folder/Placemark/Polygon");
+// split the data in the sections we want
+$placemarks = $xml->xpath("/kml/Document/Folder/Placemark");
 
+$items = array();
 
-// what's presented on the screen
-$output = "<pre>";
-
-foreach( $polygons as $polycount => $polygon ){
-	$output .= "{ coords:[";
+foreach( $placemarks as $polycount => $placemark ){
 	
-	$coordinates = $polygon->outerBoundaryIs->LinearRing->coordinates;
+	// case the type of placemark
+	
+	if( !empty($placemark->Polygon) ) {
+		// it's a polygon
+		$name = (string) $placemark->name;
+		$items[$name] = array();
+		$items[$name]["coords"] = generateCoords( $placemark->Polygon->outerBoundaryIs->LinearRing->coordinates );
+		$items[$name]["color"] = getStyle( substr( (string)$placemark->styleUrl , 1) );
+	} else if( !empty($placemark->Point) ) {
+		//it's a point
+		$name = (string) $placemark->name;
+		$items[$name]["label"] = getLabel( $placemark );
+		
+	}
+	
+	
+}
+
+// Output
+// discart the keys as we will not be needing them any more...
+print_r( json_encode( array_values($items) ) );
+
+
+
+// Functions
+
+function generateCoords( $coordinates ){
+	
+	$output = array();
+	
 	$coords = explode(",0 " , $coordinates);
 	// FIX: to pop the last element which is most likely blank
 	if( trim($coords[count($coords)-1]) == "" ){ 
@@ -22,30 +48,57 @@ foreach( $polygons as $polycount => $polygon ){
 	}
 	
 	foreach( $coords as $num => $str ){
-		
 		// split the coordinates
 		$LatLng = explode(",", $str);
 		// revese order while removing whiteaspace
-		$output .= "new LatLng(" . trim($LatLng[1]) .", ". trim($LatLng[0]) .")";
+		$output[] = array_reverse( array_trim($LatLng) );
 		
-		if( $num+1 < count($coords) ){
-			$output .= ",";
-		}
 	}
-	
-	$output .= "], " . 'colour:"lightblue" }';
-	
-	if( $polycount+1 < count($polygons) ){
-		$output .= "," . "\n";
-	} else {
-		$output .= "" . "\n";
-	}
+		
+	return $output;
 	
 }
 
-$output .= "</pre>";
+function getLabel( $placemark ){
+	
+	$name = str_replace(" ", "\n", (string)$placemark->name );
+	// order of commands: 
+	// - convert the coordinates to string
+	// - remove the last two characters defining 0 for the z-axis
+	// - explode the coordinates to array elements
+	// - reverse the order
+	$coords = array_reverse( explode(",", substr( (string)$placemark->Point->coordinates, 0, -2) ) );
+	
+	$label = array("title" => $name, "coords" => $coords);
+	return $label;
+	
+}
 
-echo $output;
+
+function getStyle( $id ){
+	global $xml;
+	// find the stylemaps related with the id
+	$stylemaps = $xml->xpath("//StyleMap[@id='". $id ."']/Pair");
+	
+	foreach( $stylemaps as $stylemap ){
+		if( $stylemap->key == "normal" ){ 
+			$style = $xml->xpath("//Style[@id='". substr($stylemap->styleUrl , 1) ."']");
+			return substr( (string)$style[0]->PolyStyle->color, 2);
+		}
+	}
+}
+
+// Helper functions
+function _trim(&$value)
+{
+    $value = trim($value);    
+}
+
+function array_trim($arr)
+{
+    array_walk($arr,"_trim");
+    return $arr;
+}
 
 
 ?>
