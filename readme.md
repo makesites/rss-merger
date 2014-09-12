1b89b8a46fb56e8b26987adde009f35ef1611ea6
brokenclock <stephane.mourey@impossible-exil.info>
2014-09-12 17:46:25 +0200

    Adding rssCacheInt interface and rssFileCache class, re-enabled the cache use in rssMerger in a new way, some renaming, and documentation, adding a feature to enable or disable the output formatting

--- readme.md
+++ readme.md
@@ -1,35 +1,100 @@
-##############################################################
+########################################################################
 
-RSS Merger v1.21
+RSS Merger v2.0.0-Pre-alpha
 
+Forked by Stéphane Mourey (stephane.mourey@impossible-exil.info)
+URL: http://impossible-exil.info
 Created by: Makis Tracend (makis@makesites.cc)
 URL: http://www.makesites.cc/projects/rss_merger
 
-##############################################################
+########################################################################
 
+
+** THIS A PRE-ALPHA VERSION !  DO NOT USE IN PRODUCTION ! **
+============================================================
 
 Description
 ===========
 This script will load a number of RSS feeds and compile them into one RSS 2.0 file. You can use it to present a mash-up of your news from different places (blogs, twitter etc.) or just present news from other sources around the Net.
 
+Usage
+=====
+Without cache :
+```php
+<?php
+require_once('rssMerger.php');
+$mymerger = new Taophp\rssMerger();
 
-Instructions
+// Set the total number of items to produce (default is all)
+$mymerger->setNumberOfItems2Produce(10);
+
+// Set the number of items to gather from each feed (default is all)
+$mymerger->setNumberOfItems2Gather(2);
+
+// Set a "nice" output (with tabulations and line breaks)
+$mymerger->formatted = true;
+
+// Adding feeds
+// with the fluent interface
+$mymerger->addRssFeeds('http://exemple.com/feed.rss')
+				 ->addRssFeeds('http://exemple.com/feed2.rss')
+// With one argument foreach new feed
+$mymerger->addRssFeeds('http://exemple.com/feed3.rss',
+											 'http://exemple.com/feed4.rss')
+// Or with an array of feeds
+$mymerger->addRssFeeds(array(
+												'http://exemple.com/feed3.rss',
+												'http://exemple.com/feed4.rss'
+											));
+
+// Get the resulting RSS feed in a string
+$rssString = $mymerger->getMerged();
+echo $rssString;
+```
+
+With file cache :
+```
+<?php
+require_once('rssMerger.php');
+require_once('rssCacheInt.php');
+require_once('rssFileCache.php');
+
+// create the cache from a directory path
+$cache = new Taophp\rssFileCache('/path/to/my/store/directory');
+// set a baseurl to redirect the client to the cached file when possible
+$cache->setBaseUrlRedirection('http://url.to/my/store/directory');
+
+$merger = new Taophp\rssMerger();
+$merger	->setCache($cache)
+				->addFeeds('http://exemple.com/feed5.rss,http://exemple.com/feed6.rss');
+
+// Get the resulting RSS feed in a string
+$rssString = $mymerger->getMerged();
+
+// check if the client was redirected to the cached file
+// should be true if setBaseUrlRedirection was called
+if ($cache->wasRedirected()) {
+	// the client was redirected, no output after this will be visible
+	die();
+}else{
+	// the client was not redirected, you should either
+	// redirect it to the new cache file
+	// or output the content
+	// or what you wanna do
+	$cache->redirect($merger->getFeedId());
+}
+```
+
+Requirements
 ============
-Put in the RSS feeds you want to parse in the "rss_list.txt" (one URL per line) and the "rss_merger.php" script will output a new rss feed with a selection of items from all the RSS feeds. 
-
-It works fine right out of the box but you may want to edit some of the global variables defined in the beginning of the script. Just open it in a text editor and fire away... 
-
-$site_name 		: Replace this with your website's title
-$site_url 		: Replace this with your website's URL
-$site_info 		: A small description of your site or this feed's purpose
-$rss_list 		: A text file with all your feed URLs listed, one per line
-$cached_rss 	: The cache RSS file that will be created to improve performance
-$xml_encoding 	: The encoding you want the final XML file to have
-$num_of_items 	: The number of items it will collect from each feed, counted off the top of the RSS feed. 
-
+PHP >= 5.3
 
 Changelog
 =========
+12-09-2014	(v2.0.1-Pre-alpha) Adding rssCacheInt interface and rssFileCache class, re-enabled the cache use in rssMerger in a new way, some renaming, and documentation, adding a feature to enable or disable the output formatting
+
+11-09-2014	(v2.0.0-Pre-alpha) Move the code to my own pseudo "Coding Standard", inhibate the use of the cache
+
 23-05-2009 	(v1.21) Removed extra information before the <rss> tag to better support feedburner
 
 14-06-2008 	(v1.2) 	Re-written parsing function for PHP 5 using the SimpleXML extension. Items now sorted by date.
@@ -37,7 +102,7 @@ Changelog
 01-02-2008 	(v1.1) 	Bug fix: Absence of a description or title for the XML file could break structure of the final XML output.
 					Improvement: Made the description of the final XML output a variable.
 
-27-01-2008 	(v1.0) 	Initial release: Basic parsing & caching functionality created. 
+27-01-2008 	(v1.0) 	Initial release: Basic parsing & caching functionality created.
 
 
 Copyright
---
+++ rssMerger.php
@@ -0,0 +1,224 @@
+<?php
+namespace Taophp;
+/**
+ * This file defile the rssMerger class
+ *
+ * The rssMerger class is the main to use the Rss-merger package
+ * @package Rss-merger
+ * @license GPLv2
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @copyright 2014 Stéphane Mourey <stephane.mourey@impossible-exil.info>
+ * @author Stéphane Mourey <stephane.mourey@impossible-exil.info>
+ * @copyright 2009-2011 Makis Tracend <makis@makesites.cc>
+ * @author Makis Tracend
+ * @version 2.0.1-pre-alpha Fork-Day-One
+ * */
+
+class rssMerger {
+	const SCRIPT_VERSION = '2.0.1-Pre-alpha';
+	const SCRIPT_NAME = 'Rss Merger';
+	const SCRIPT_URL = 'https://github.com/taophp/rss-merger';
+
+	/** @type string $siteName website title */
+	public $siteName;
+	/** @type string $siteUrl website url */
+	public $siteUrl;
+	/** @type string $feedDesc small description of this feed's purpose */
+	public $feedDesc;
+	/** @type array list of urls of  to gather */
+	public $rssList;
+	/** @type string Encoding to use for the feed */
+	public $xmlEncoding = 'UTF-8';
+	/** @type string the language of the producted feed */
+	public $lang = 'en';
+	/** @type int Max number of items to gather from each feed, 0 for all */
+	protected $nbItems2Gather = 0;
+	/** @type int Max number of items to produce in the result, 0 for all */
+	protected $nbItems2Produce = 0;
+	/** @type object the cache object to store data */
+	protected $cache;
+	/** @type bool set to true if you want the feed output to be formatted (i.e. with tabulations and linebreaks) */
+	public $formatted = false;
+
+	/**
+	 *	Set the number of items to gather from each feed
+	 *
+	 * @param int $nbItems Number of items to gather from each feed, 0 for all
+	 *
+	 * @return rssMerger $this
+	 *
+	 * */
+	public function setNumberOfItems2Gather($nbItems)
+	{
+		$this->nbItems2Gather = $nbItems;
+		return $this;
+	}
+
+	/**
+	 *	Set the number of items to output in the feed
+	 *
+	 * @param int $nbItems Number of items to output in the feed, 0 for all
+	 *
+	 * @return rssMerger $this
+	 *
+	 * */
+	public function setNumberOfItems2Produce($nbItems)
+	{
+		$this->nbItems2Produce = $nbItems;
+		return $this;
+	}
+
+	/**
+	 *	Add feeds to the list of the feeds to grab
+	 *
+	 * @param string|array $feeds the list of the feeds to add, in comma separated string or in an array
+	 *
+	 * @return rssMerger $this
+	 *
+	 * */
+	public function addRssFeeds($feeds) {
+		$args = func_get_args();
+		if (count($args)>1) $feeds = $args;
+		if (is_string($feeds) && strpos($feeds,',')) $feeds = explode(',',$feeds);
+		if (is_array($feeds))
+			array_walk($feeds,array($this,__METHOD__));
+		else {
+			$this->rssList[] = $feeds;
+		}
+		return $this;
+	}
+
+	/**
+	 * The main method of the class, doing the merge
+	 *
+	 * @return string the new RSS feed
+	 * */
+	public function getMerged(){
+		if ($this->checkCache()) {
+			return $this->cache->getRSSCache($this->getFeedId());
+		}
+		return $this->gatherNews();
+	}
+
+	/**
+	 * Check if the cache should be use and is usable
+	 *
+	 * @return bool
+	 * */
+	protected function checkCache() {
+		if(!$this->cache) return false;
+		return $this->cache->checkCache($this->getFeedId());
+	}
+
+	/**
+	 * Loop through the rss URLs and gather the items
+	 *
+	 * @return string the new RSS feed
+	 * */
+	protected function gatherNews() {
+		$rssItems = array();
+
+		foreach ($this->rssList as $rssUrl) {
+		$xml = simplexml_load_file($rssUrl, null, false);
+		if($xml){
+			$feedNbItems = count($xml->channel->item);
+			$maxNum = $this->nbItems2Gather ? min($feedNbItems, $this->nbItems2Gather) : $feedNbItems;
+			for ($i=0; $i< $maxNum; $i++) {
+				$item = $xml->channel->item[$i];
+				$new['title'] = $item->title;
+				$new['link'] = $item->link;
+				$new['description'] = $item->description;
+				$new['pubDate'] = $item->pubDate;
+				$new['guid'] = $item->guid;
+				$new['date'] = strtotime($item->pubDate);
+				array_push($rssItems, $new );
+			}
+		}
+	}
+		//sort the items according to date
+		usort($rssItems, array(__CLASS__, 'sortByDate'));
+		if ($this->nbItems2Produce > 0)
+			$rssItems = array_slice($rssItems,0,$this->nbItems2Produce);
+		return $this->outputXML($rssItems);
+	}
+
+	/**
+	 *	Use to sort items, callback function for usort
+	 *
+	 * @param array $a rss item
+	 * @param array $b rss item
+	 *
+	 * @return int (1 if $a is older, -1 if $b is older, 0 otherwise)
+	 * */
+	protected static function sortByDate($a,$b) {
+		if ($a['date'] < $b['date']) return 1;
+		if ($a['date'] > $b['date']) return -1;
+		return 0;
+	}
+
+	/**
+	 *	Produce the RSS final string
+	 *
+	 * @param array $rssItems the array containing all the items to output
+	 *
+	 * @return string the RSS formatted string with all that stuff
+	 * */
+	protected function outputXML($rssItems) {
+		$t=$n='';
+		if ($this->formatted) {
+			$t="t";
+			$n="n";
+		}
+		$output = '<?xml version="1.0" encoding="' . $this->xmlEncoding . '"?>' . $n;
+		$output .= '<rss version="2.0">' . $n;
+		$output .= $t . '<channel>' . $n;
+		$output .= $t.$t . '<title>' . $this->siteName . '</title>' . $n;
+		$output .= $t.$t . '<link>' . $this->siteUrl . '</link>' . $n;
+		$output .= $t.$t . '<description>' . $this->feedDesc . '</description>' . $n;
+		$output .= $t.$t . '<pubDate>' . date(DATE_RFC822) . '</pubDate>' . $n;
+		$output .= $t.$t . '<generator>'.self::SCRIPT_NAME.' v' . self::SCRIPT_VERSION . ' : '.self::SCRIPT_URL.' </generator>' . $n;
+		$output .= $t.$t . '<language>en</language>' . $n;
+
+		foreach ($rssItems as $item) {
+			$output .= $t.$t . '<item>' . $n;
+			$output .= $t.$t.$t . '<title>' . $item['title'] . '</title>' . $n;
+			$output .= $t.$t.$t . '<link>' . $item['link'] . '</link>' . $n
+			$output .= $t.$t.$t . '<description><![CDATA[' . $item['description'] . ']]></description>' . $n;
+			$output .= $t.$t.$t . '<pubDate>' . $item['pubDate'] . '</pubDate>' . $n;
+			$output .= $t.$t . '</item>' . $n.$n;
+		}
+		$output .= $t . '</channel>' . $n;
+		$output .= '</rss>';
+
+		// create the cache file for later use...
+		if ($this->cache) $this->cache->feedRSSCache($this->getFeedId(),$output);
+
+		return $output;
+	}
+
+	/**
+	 *	Produce a unique Id for $this, mainly to use with the cache
+	 *
+	 * @return string the Id for $this
+	 * */
+	public function getFeedId(){
+			return md5(serialize($this));
+	}
+
+	/**
+	 *	Attach a cache objet to use, the object must implements the rssCacheInt interface
+	 *
+	 * @param object $cache the object to use as cache
+	 *
+	 * @return rssMerger $this
+	 * */
+	public function setCache(object $cache)
+	{
+		if (!in_array('rssCacheInt',class_implements($cache))
+			throw new Exception('rssMerger::setCache expects an objet that implements the rssCaheInt interface.');
+		$this->cache = $cache;
+		return $this;
+	}
+
+
+}
